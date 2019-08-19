3d162cdbea25af1edef2ce6509735e792a530dbd
Max Rumsey <max@maxrumsey.xyz>
2019-08-19 18:30:07 +0800

    Adding 'setup' command

---
+++ commands/config/setup.js
@@ -0,0 +1,44 @@
+module.exports = async (msg, command, args, config) => {
+  const messageFilter = m => m.author.id == msg.author.id;
+
+  const admin = await global.Database.isAdmin(msg.member, config);
+  if (!admin) return msg.reply('Sorry, you do not have the administrator role or the Manage Server permission.')
+
+  msg.reply("What channel do you want rules to appear in?");
+  let rulesChannel = await msg.channel.awaitMessages(messageFilter, {
+    maxMatches: 1,
+    time: 10000
+  })
+  rulesChannel = rulesChannel.first();
+  if (!rulesChannel) return msg.reply('Cancelling.')
+  let channel = rulesChannel.mentions.channels.first()
+  if (!channel) return msg.reply('Channel not found.')
+  if (channel.guild.id != msg.guild.id) return msg.reply('Channel not found.');
+  await global.Database.query('UPDATE `servers` SET channel_rules_id = ? WHERE (server_id = ?)', [channel.id, msg.guild.id])
+
+  msg.reply("What channel do you want the votes to appear in?");
+  let votesChannel = await msg.channel.awaitMessages(messageFilter, {
+    maxMatches: 1,
+    time: 10000
+  })
+  votesChannel = votesChannel.first();
+  if (!votesChannel) return msg.reply('Cancelling.')
+  channel = votesChannel.mentions.channels.first()
+  if (!channel) return msg.reply('Channel not found.')
+  if (channel.guild.id != msg.guild.id) return msg.reply('Channel not found.');
+  await global.Database.query('UPDATE `servers` SET channel_vote_id = ? WHERE (server_id = ?)', [channel.id, msg.guild.id])
+
+  return msg.reply('Done! Now run `d!help` for a list of commands.')
+}
+/*
+const channel = msg.mentions.channels.first();
+if (!channel) return msg.reply('Channel not found.')
+if (channel.guild.id != msg.guild.id) return msg.reply('Channel not found.');
+if (args[0] == 'vote') {
+  await global.Database.query('UPDATE `servers` SET channel_vote_id = ? WHERE (server_id = ?)', [channel.id, msg.guild.id])
+} else if (args[0] == 'rules') {
+  await global.Database.query('UPDATE `servers` SET channel_rules_id = ? WHERE (server_id = ?)', [channel.id, msg.guild.id])
+} else {
+  return msg.reply('Channel Name Not Recognized.')
+}
+*/
--- framework/handlers.js
+++ framework/handlers.js
@@ -23,9 +23,8 @@ exports.join = (guild) => {
   if (defaultChannel) {
     defaultChannel.send([
       'Hi, thanks for inviting me into your server! To start off, you\'d want to set a channel were rules and votes will be posted',
-      'You can do that with the following commands:',
-      '```d!setChannel vote #vote_for_rules_here\nd!setChannel rules #rules```',
-      'The rule creation process is all automated, and easy to use.',
+      'You can do that by running the command `d!setup`!',
+      'The rule creation process is all automated, and easy to use. Just set it and forget it.',
       'If you run into any problems, you can type d!help for a list of commands and d!debug to debug the bot if it is not working correctly.'
     ].join('\n'))
   }
