75c3561b25cbed802a4c2672879b169b7ac3bc79
Max Rumsey <max@maxrumsey.xyz>
2019-08-19 18:33:06 +0800

    Revert "Revert "Adding 'setup' command""
    This reverts commit 97783cebaa353a1a1bd2c49d4cce0787d2262b0b.

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -21,6 +21,10 @@ module.exports = (msg, command, args) => {
         {
           "name": "d!setConfig",
           "value": "Change a configuration value. [Admin Use Only]\nd!setConfig"
+        },
+        {
+          "name": "d!setup",
+          "value": "Setup the bot for first time use. [Admin Use Only]\nd!setup"
         }
       ]
     },
