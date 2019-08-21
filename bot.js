20966657ff7126d635c8ea2dd3e27c161d5ffc00
Max Rumsey <max@maxrumsey.xyz>
2019-08-21 09:29:53 +0800

    Optimisations.

--- bot.js
+++ bot.js
@@ -3,7 +3,18 @@ if (!process.env.TOKEN) {
 }
 
 const discord = require('discord.js');
-const Client = global.client = new discord.Client();
+const Client = global.client = new discord.Client({disabledEvents: [
+            "GUILD_SYNC",
+            "CHANNEL_PINS_UPDATE",
+            "USER_NOTE_UPDATE",
+            "RELATIONSHIP_ADD",
+            "RELATIONSHIP_REMOVE",
+            "USER_SETTINGS_UPDATE",
+            "VOICE_STATE_UPDATE",
+            "VOICE_SERVER_UPDATE",
+            "TYPING_START",
+            "PRESENCE_UPDATE"
+        ]});
 
 const framework = require('./framework/');
 
